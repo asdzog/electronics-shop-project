a89d8c5d43b777250d795313185d1e9e329190ef
Aleksey <asdzog@mail.ru>
2023-09-22 16:53:26 +0400

    add new methods instantiate_from_csv and string_to_number and change attribute name in class Item

--- src/item.py
+++ src/item.py
@@ -1,3 +1,7 @@
+from csv import DictReader
+import os
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -18,6 +22,17 @@ class Item:
         self.quantity = quantity
         self.all.append(self)
 
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        if len(name) > 10:
+            self.__name = name[:10]
+        else:
+            self.__name = name
+
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
@@ -31,3 +46,29 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= Item.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls, csv_file):
+        """
+        Инициализирует экземпляры класса данными из файла items.csv
+        """
+        Item.all = []
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+        items_file_path = os.path.join(current_dir, csv_file)
+        with open(items_file_path, 'r', encoding='windows-1251') as csv:
+            items = DictReader(csv)
+            for product in items:
+                name = product['name']
+                price = Item.string_to_number(product['price'])
+                count = Item.string_to_number(product['quantity'])
+                Item(name, price, count)
+
+    @staticmethod
+    def string_to_number(text_line):
+        """
+        Возвращает число из строки, если возможно, иначе - None
+        """
+        try:
+            return int(float(text_line))
+        except ValueError:
+            return None
